59fedc1d21220447b8631cbc4e82124238bbb8ff
Jamile Moreira <jamilemoreira2310@gmail.com>
2024-01-04 18:14:23 -0300

    exercicio 4

--- 04/scripts.ts
+++ 04/scripts.ts
@@ -1,40 +1,23 @@
 const geradorEtiquetas = (info: {
-    produto: string,
-    lote: number,
-    ano: number,
-    qtd: number
+  produto: string
+  lote: number
+  ano: number
+  qtd: number
 }): string[] => {
-    const resultado: string[] = [];
+  const resultado: string[] = []
 
-    for (let i = 1; i <= info.qtd; i++) {
-        resultado.push(`${info.lote}-${info.ano}-${i.toString().padStart(3, '0')}`);
-    }
+  for (let i = 1; i <= info.qtd; i++) {
+    resultado.push(`${info.lote}-${info.ano}-${i.toString().padStart(3, '0')}`)
+  }
 
-    return resultado;
+  return resultado
 }
 
-console.log(geradorEtiquetas({
+console.log(
+  geradorEtiquetas({
     produto: 'CPU Dual Core 3.0GHZ',
     lote: 321,
     ano: 2022,
-    qtd: 5
-}));
-
-
-
-
-
-// {
-//     produto: 'CPU Dual Core 3.0GHZ',
-//     lote: 321,
-//     ano: 2022,
-//     qtd: 5
-// }
-
-// ;[
-//     "321-2022-001",
-//     "321-2022-002",
-//     "321-2022-003",
-//     "321-2022-004",
-//     "321-2022-005",
-// ]
+    qtd: 5,
+  })
+)
