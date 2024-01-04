72eb0933c6d7bd80b4542fa5cfa2f3edfb9787e8
Jamile Moreira <jamilemoreira2310@gmail.com>
2024-01-04 18:03:13 -0300

    exercicio 3

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -1,17 +1,17 @@
 const tabuadas = (numeros: number[]): string => {
-    let resultado: string = '';
+  let resultado: string = ''
 
-    for (const n of numeros) {
-        for (let i = 0; i <= 10; i++) {
-            resultado += `${n} x ${i} = ${n * i} \n`;
+  for (const n of numeros) {
+    for (let i = 0; i <= 10; i++) {
+      resultado += `${n} x ${i} = ${n * i} \n`
 
-            if (i === 10) {
-                resultado += '------------------- \n'
-            }
-        }
+      if (i === 10) {
+        resultado += '------------------- \n'
+      }
     }
+  }
 
-    return resultado;
+  return resultado
 }
 
-console.log(tabuadas([1, 7, 9, 5]));
+console.log(tabuadas([1, 2, 5]))
